214e7eab2f5eef29549cb723dcecdd7e076810df
Ali Saleh <alisaleh06@outlook.com.au>
2024-10-02 17:50:33 +1000

    Designed API Endpoints

--- server/server.js
+++ server/server.js
@@ -3,9 +3,26 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-// HABIT TASKS: Get, Post, Patch Delete
-// WEEKLY TASKS: Get, Post, Patch, Delete
-// BIG GOAL: Get, Post, Patch, Delete
+/*
+
+Endpoints:
+  GET /api/tasks/daily -> Gets daily tasks
+  POST /api/tasks/daily -> Insert daily task(s)
+  PATCH /api/tasks/daily/:taskID -> Change a daily task
+  DELETE /api/tasks/daily/:taskID -> Delete a daily task
+
+  GET /api/tasks/weekly -> Gets weekly tasks
+  POST /api/tasks/weekly -> Insert weekly task(s)
+  PATCH /api/tasks/weekly/:taskID -> Change a weekly task (Includes task & weight)
+  DELETE /api/tasks/weekly/:taskID -> Delete a weekly task
+
+  GET /api/goals -> Gets goals
+  GET /api/goals/:year -> Gets goal for specific year (after a patch request, perhaps)
+  POST /api/goals/:year -> Inserts a goal for the specific year OR adds a year
+  PATCH /api/goals/:year -> Changes a goal for the specific year
+  DELETE /api/goals/:year -> Deletes a year from the goals
+
+*/
 
 app.listen(port, () => {
   console.log("Listening on port: " + port);
